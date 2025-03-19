cdf99f97d9283d3ce8991f1c3d55b89907178c68
LucasABCorrea <137966697+LucasABCorrea@users.noreply.github.com>
2025-03-19 18:42:15 -0300

    git add: funcção promover

--- src/PrimeirosPassosPOO/exercicio04/Main.java
+++ src/PrimeirosPassosPOO/exercicio04/Main.java
@@ -13,14 +13,15 @@ public class Main {
 
     public static void inicializarMenu() {
         int op = 0, cont = 0;
-        while (op != 4) {
+        while (op != 5) {
 
             op = 0;
             System.out.println("Escolha uma opção: ");
             System.out.println("1. Cadastrar");
             System.out.println("2. Pesquisar");
             System.out.println("3. Listar");
-            System.out.println("4. Finalizar");
+            System.out.println("4. Promover");
+            System.out.println("5. Finalizar");
             op = sc.nextInt();
 
             if (op == 1) {
@@ -41,8 +42,19 @@ public class Main {
             } else if (op == 3) {
                 listarFuncionarios(cont);
             } else if (op == 4) {
+                sc.nextLine();
+                String nome, cargo;
+                System.out.println("Insira o nome do funcionário que deseja buscar: ");
+                nome = sc.nextLine();
+                if (pesquisarFuncionario(nome, funcionarios, cont)) {
+                    System.out.println("Insira o cargo que deseja atribuir a esse funcionário: ");
+                    cargo = sc.nextLine();
+
+                    promoverFuncionario(nome, funcionarios, cont, cargo);
+                }
+            } else if (op == 5) {
                 System.out.println("\nFim do programa!");
-            } else if (op < 1 || op > 4) {
+            } else if (op < 1 || op > 5) {
                 System.out.println("Opção inválida!!!");
             }
         }
@@ -63,23 +75,35 @@ public class Main {
 
     }
 
-    public static void pesquisarFuncionario(String nome, Funcionario[] funcionarios, int cont) {
+    public static void promoverFuncionario(String nome, Funcionario[] funcionarios, int cont, String cargo) {
+        for (int i = 0; i < cont; i++) {
+            if (funcionarios[i].nome.equalsIgnoreCase(nome)) {
+                funcionarios[i].cargo = cargo;
+                System.out.println("Funcionário promovido");
+            }
+        }
+
+    }
+
+    public static boolean pesquisarFuncionario(String nome, Funcionario[] funcionarios, int cont) {
         boolean check = false;
         int aux = 0;
         for (int i = 0; i < cont; i++) {
             if (funcionarios[i].nome.equalsIgnoreCase(nome)) {
                 check = true;
                 aux = i;
-            }
-            if (check) {
-                System.out.println("O funcionário está cadastrado!");
-                System.out.println(funcionarios[aux].nome);
-                System.out.println(funcionarios[aux].cargo);
-                System.out.println("R$" + String.format("%.2f", funcionarios[aux].salario));
-            } else {
-                System.out.println("O funcionário não está cadastrado!");
+                break;
             }
         }
+        if (check) {
+            System.out.println("O funcionário está cadastrado!");
+            System.out.println(funcionarios[aux].nome);
+            System.out.println(funcionarios[aux].cargo);
+            System.out.println("R$" + String.format("%.2f", funcionarios[aux].salario));
+        } else {
+            System.out.println("O funcionário não está cadastrado!");
+        }
+        return check;
     }
 
     public static void listarFuncionarios(int cont) {
